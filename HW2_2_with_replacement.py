831b0880d18edfadb10ab817ec938d6ba0d28291
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-02-16 14:50:40 -0500

    Update HW2_2_with_replacement.py

--- HW2_2_with_replacement.py
+++ HW2_2_with_replacement.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 N = 10000 # number of experiments
@@ -8,52 +7,39 @@ redballs   = 4*[1]
 blueballs  = 5*[2]
 greenballs = 6*[3]
 
-balls = redballs + blueballs + greenballs
-
-totalballs = N*[balls]
-
 counta = 0
 countb = 0
 countc = 0
 ballsdrawn = 3
 
-'''
-for i in range(0,N):
-    balls = redballs + blueballs + greenballs
-    print(balls)
-    balls.remove(1)
-    print(balls)
 
-'''
-
-
-for i in range(0,N):
-    balls = redballs + blueballs + greenballs
-    ball1 = random.choices(balls,k=1)
+for i in range(0,N):                            #loop removes balls
+    balls = redballs + blueballs + greenballs   #selected then checks
+    ball1 = random.choices(balls,k=1)           #conditions for tallying
     balls.remove(ball1[0])
-    #print(ball1 , balls)
+
     ball2 = random.choices(balls,k=1)
     balls.remove(ball2[0])
-    #print(ball2 , balls)
+
     ball3 = random.choices(balls,k=1)
     balls.remove(ball3[0])
-    #print(ball3 , balls)
+
     selections = [ball1 , ball2 , ball3]
     
     if selections[0] == selections[1] == [color] or \
        selections[1] == selections[2] == [color] or \
-       selections[2] == selections[0] == [color]:
-       counta = counta + 1
+       selections[2] == selections[0] == [color]:     #check for
+       counta = counta + 1                            #2/3 red
        
-    if selections[0]==selections[1]==selections[2]:
-       countb = countb + 1
+    if selections[0]==selections[1]==selections[2]: #checks for
+       countb = countb + 1                          #all same color
 
     if selections[0] != selections[1] and \
        selections[1] != selections[2] and \
-       selections[2] != selections[0]:
-        countc = countc +1
+       selections[2] != selections[0]:              #checks for
+        countc = countc +1                          #all diff color
 
 
-print(counta/N) # 2/3 red
-print(countb/N) # 3/3 same color
-print(countc/N) # all diff colors
+print(counta/N) # 2/3 red          0.1501
+print(countb/N) # 3/3 same color   0.0763
+print(countc/N) # all diff colors  0.2627
