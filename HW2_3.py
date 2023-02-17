2ebbeccdacff63f70c41325d4ccd4fe0541df830
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-02-16 21:54:04 -0500

    adding probability equation

--- HW2_3.py
+++ HW2_3.py
@@ -1,29 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  8 11:07:46 2023
-
-@author: John
-"""
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 
 
 coin =[-1,1]
 exp =10000
 steps = 100
+stepstaken = 100
 
-count=0
+
 finalpos = []
 
 for i in range(0,exp):
     trial = sum(random.choices(coin,k=steps))
     finalpos.append(trial)
+    
+    
+for i in range(0,steps):
+    stepsfromorigin = steps
+    for a in range(0,stepsfromorigin):
+        if float.is_integer((steps-stepsfromorigin)/2)  or \
+            float.is_integer((steps+stepsfromorigin)/2):
+                math.factorial(steps)/(math.factorial((steps-stepsfromorigin)/2) + math.factorial((steps-stepsfromorigin)/2))
+        
+    
 
 
-y,x,_ =plt.hist(finalpos,bins= np.linspace(-20, 20, num=41),range=(-10,10),align='left')
+y,x,_ =plt.hist(finalpos,bins= np.linspace(-40, 40, num=100),range=(-10,10),align='mid')
 #plt.xticks(list(range(-10,10)))
 plt.title('final positions from 10000 random walk of 100 steps')
 plt.xlabel('final positions')
