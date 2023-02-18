8fc53c914f358a4d9d74aabb4562ccf3b6119f65
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-02-17 23:45:30 -0500

    trying to add theoretical equation

--- HW2_3.py
+++ HW2_3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
 import random
 import math
 
@@ -7,6 +8,8 @@ import math
 coin =[-1,1]
 exp =10000
 steps = 100
+oddsteps = [*range(1,steps,2)]
+evensteps = [*range(0,steps,2)]
 stepstaken = 100
 
 
@@ -16,20 +19,32 @@ for i in range(0,exp):
     trial = sum(random.choices(coin,k=steps))
     finalpos.append(trial)
     
+ 
+'''   
+ 
+evenfinalpos = []
+theofinalprob = []
+for i in range(0,steps//2):
+    oddstepsfromorigin = oddsteps
+    evenstepsfromorigin = evensteps
+    for a in range(0,i):
+        even = (0.5**i)*math.factorial(evensteps[i])//(math.factorial((evensteps[a]-evenstepsfromorigin[a])//2) + math.factorial((evensteps[a]-evenstepsfromorigin[a])//2))
+        theofinalprob.append(even)
     
-for i in range(0,steps):
-    stepsfromorigin = steps
-    for a in range(0,stepsfromorigin):
-        if float.is_integer((steps-stepsfromorigin)/2)  or \
-            float.is_integer((steps+stepsfromorigin)/2):
-                math.factorial(steps)/(math.factorial((steps-stepsfromorigin)/2) + math.factorial((steps-stepsfromorigin)/2))
-        
+    
+'''    
     
 
+counts,bins,bars = plt.hist(finalpos,bins= np.linspace(-40, 40, num=100),range=(-10,10),align='mid', density = True,stacked = True)
+print(max(counts))
 
-y,x,_ =plt.hist(finalpos,bins= np.linspace(-40, 40, num=100),range=(-10,10),align='mid')
-#plt.xticks(list(range(-10,10)))
+counts,bins,bars = plt.hist(finalpos,bins= np.linspace(-40, 40, num=100),range=(-10,10),align='mid')
+print(max(counts)/10000)
+
 plt.title('final positions from 10000 random walk of 100 steps')
 plt.xlabel('final positions')
 plt.ylabel('count of final positions')
+
+
+
 
