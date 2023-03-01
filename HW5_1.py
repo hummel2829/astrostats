adc803e5d6bb3800994d777d1ce49eadff0afa4f
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-02-28 22:25:56 -0500

    redefined fucntions draft2

--- HW5_1.py
+++ HW5_1.py
@@ -19,11 +19,19 @@ for i in range (0,experiments):
     nsuc.append(sum(list(filter(lambda x: (x == 1), sample))))
     psuc.append(sum(list(filter(lambda x: (x == 1), sample)))/len(sample))
 
-def number(n):
+def prob(n):
     return nsuc.count(n)/experiments
 
+result = map(prob,success)
+
+Pdsuc = list(result)       #prob dist of success from sim
+
+def number(n):
+    return nsuc.count(n)
+
 result = map(number,success)
-Pdsuc = list(result)
+
+ndsuc = list(result)
 
 
 def PB(k):
@@ -38,18 +46,15 @@ theoPB = list(result)
 
 figure, axis = plt.subplots(1, 1,constrained_layout=True)
 
+axis.bar(success[35:45],Pdsuc[35:45], color = 'w', edgecolor = 'b' , width = 0.4, label='Sim')
+axis.scatter(success[35:45],theoPB[35:45], s=20, c='r', marker="o", label='PB(k)')
 
-
-
-#axis.bar(nsuc,psuc, color = 'w', edgecolor = 'b' , width = 0.4, label='Sim')
-axis.scatter(success,theoPB, s=20, c='r', marker="o", label='PB(k)')
-
-#axis.legend(loc='upper right')
-
-
+axis.legend(loc='upper right')
 
 font = {'fontname' : 'Times New Roman' , 'size' : 20}
-axis.set_title('Solar flares over 1000 days',**font)
+axis.set_title('avg = %1.3f' %avg + ' , var = %1.3f' %stdN ,**font)
+
+#axis.set_title('Solar flares over 1000 days',**font)
 axis.set_xlabel('successes',**font)
 axis.set_ylabel('P',**font)
 
