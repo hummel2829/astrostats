43f07591c26a1c7bfb042958e2d067f34aa6a5ae
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-03-02 09:46:17 -0500

    final draft

--- HW5_3.py
+++ HW5_3.py
@@ -12,28 +12,54 @@ from math import sqrt
 mean = 10
 std = 1
 n = 10
+experiments = 10000
 
 G10 = np.random.normal(mean,std,n)
 meanG10 = np.mean(G10)
 
 
-interval74 = (meanG10 + (1.96*std/sqrt(n))) - (meanG10 - (1.96*std/sqrt(n)))
+interval74 = [(meanG10 + (1.96*std/sqrt(n))) , (meanG10 - (1.96*std/sqrt(n)))]
 
 
 stdsample = np.std(G10)
 df = n-1
 t = 2.262 #from t critical value table in textbook
 
-interval715 = (meanG10 + (t*stdsample/sqrt(n))) - (meanG10 - (t*stdsample/sqrt(n)))
+interval715 = [(meanG10 + (t*stdsample/sqrt(n))) , (meanG10 - (t*stdsample/sqrt(n)))]
 
 print(interval74 , interval715)
 
-UB74 = meanG10 + (1.96*std/sqrt(n))
-LB74 = meanG10 - (1.96*std/sqrt(n))
+
+count = 0
+
+for i in range(0,experiments):
+    G10 = np.random.normal(mean,std,n)
+    UB74 = np.mean(G10) + (1.96*std/sqrt(n))
+    LB74 = np.mean(G10) - (1.96*std/sqrt(n))
+    if LB74 <= mean <= UB74:
+        count = count + 1
+
+fracininterval = count/experiments
+print(fracininterval)
 
 
-G10 = np.random.normal(mean,std,n)
-if LB74 <= mean <= UB74
+for i in range(0,experiments):
+    G10 = np.random.normal(mean,std,n)
+    UB = (np.mean(G10) + (t*np.std(G10)/sqrt(n)))
+    LB = (np.mean(G10) + (t*np.std(G10)/sqrt(n)))
+    if LB <= mean <= UB:
+        count = count + 1
+
+fracininterval = count/experiments
+print(fracininterval)
+
+
+
+
+
+
+
+
 
 
 
