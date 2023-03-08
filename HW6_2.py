2be7247049f1b7516575136c21a9e63b27b5aa62
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-03-08 13:52:57 -0500

    downward trend in avg xray

--- HW6_2.py
+++ HW6_2.py
@@ -35,16 +35,47 @@ flares = allminutes.astype(int)
 
 mininday = int(24*60)
 
-flares.resize(mininday,ceil(minfrom1stflare[-1]/mininday))
+flaresdayarray = np.copy(flares)
+flaresdayarray.resize(ceil(minfrom1stflare[-1]/mininday),mininday)
 
-flaresrowsum = np.sum(flares,axis=1)
+flaressuminday = np.sum(flaresdayarray,axis=1)
+
+flaresweekarray = np.copy(flaresdayarray)
+mininweek = 7*mininday
+flaresweekarray.resize(ceil(minfrom1stflare[-1]/mininweek),mininweek)
+
+flaressuminweek = np.sum(flaresweekarray,axis=1)
 
 
+flares2weekarray = np.copy(flaresdayarray)
+minin2week = 14*mininday
+flares2weekarray.resize(ceil(minfrom1stflare[-1]/minin2week),minin2week)
+
+flaresavgin2week = np.average(flares2weekarray,axis=1)
+
+
+
+
+
+monthinyr = 31
+flaresinmonth = np.copy(flaressuminday)
+flaresinmonth.resize(ceil(len(flaressuminday)/monthinyr),monthinyr)
+flaresmonthsum = np.sum(flaresinmonth,axis=1)
+flaresavginmonth = np.average(flaresinmonth, axis =1)
+
+months = np.arange(0,flaresavginmonth.shape[0])
+
+a,b = np.polyfit(months, flaresavginmonth,1)
 
 
 figure, axis = plt.subplots(1, 1,constrained_layout=True)
 
-axis.plot(flaresrowsum, 'o')
+axis.scatter(months, flaresavginmonth, s=20, c='r', marker="o", label='exp(k)')
+axis.plot(months, months*a +b)
+#axis.plot(flaresavginmonth, 'or')
+#axis.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+#axis.plot(flaressuminday, 'or')
+#axis.plot(flaresavgin2week, 'or')
 #axis.bar(timediffcount,timediffprob, color = 'b', edgecolor = 'b' , width = 0.4, label='Sim')
 #axis.scatter(flaresrowsum,, s=20, c='r', marker="o", label='exp(k)')
 #axis.scatter(successes[0:100],Pp, s=20, c='g', marker="o", label='Pp(k)')
