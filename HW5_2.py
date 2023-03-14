38f9633508d1f76d72a016e15f07c0bfe018afda
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-03-14 17:59:23 -0400

    Pp PB and Pe all wrong?

--- HW5_2.py
+++ HW5_2.py
@@ -1,15 +1,11 @@
 
-
 import random
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 from math import factorial
 from math import exp
-
-
-
-################### use np array of 1s and 0s NOT list 
+from math import ceil
 
 
 
@@ -20,17 +16,80 @@ xraydata = xraydata.astype(int)
 
 rows =[*range(0,xraydata.shape[0])]
 
+
 dates = [datetime.datetime(*x) for x in xraydata]
-successes = [*range(0,len(dates))]
+success = [*range(0,len(dates))]
+
+#minutes from first flare
+minfrom1stflare = [(((dates[x] - dates[0]).days)*24*60) + ((dates[x] - dates[0]).seconds)//60 for x in rows]
+
+allminutes = np.zeros(minfrom1stflare[-1]+1)
+
+np.put(allminutes, minfrom1stflare, 1)
+
+flares = allminutes.astype(int)
+
+mininday = int(24*60)
+
+flaresdayarray = np.copy(flares)
+flaresdayarray.resize(ceil(minfrom1stflare[-1]/mininday),mininday)
+
+flaressuminday = np.sum(flaresdayarray,axis=1)
+w = flaressuminday/len(flaressuminday)
+flaredayhist = np.histogram(flaressuminday,bins=len(flaressuminday),weights = w)
 
 
-cc = np.diff(dates)
-timediff = [(x.seconds) for x in cc]
-timediffhours = [x/3600 for x in timediff]
-timediffcount = [timediffhours.count(x) for x in timediffhours]
-totalflarecount = len(timediff)
+#Binomial dist
+N = int(np.max(flaredayhist[1])+1)
+p = np.max(flaredayhist[0])
+#p = np.sum(flares)/len(allminutes)
+def PB(k):
+    return((factorial(N)/(factorial(k)*factorial(N-k)))*(p**k)*(1-p)**(N-k))
+theoPB = list(map(PB,success[0:N]))
+#theoPB = list(result)
 
 
+# poisson
+def Pp(k):
+    return(((p*N)**k)*(exp(-p*N))/factorial(k))
+theoPp = list(map(Pp,success[0:N]))
+
+#exponential
+
+y = np.sum(flares)/(len(allminutes)/(mininday))
+def Pe(k):
+    return(y*(exp(-y*k)))
+theoPe = list(map(Pe,success[0:N]))
+
+
+
+
+
+figure, axis = plt.subplots(1, 1,constrained_layout=True)
+
+
+#hist = axis.hist(flaressuminday,bins = int(np.max(flaredayhist[1])), weights = w, color = 'w', edgecolor = 'b' , label='Sim')
+#axis.plot(success[0:N],theoPB, c='r', marker="o", label='PB(k)')
+#axis.plot(success[0:N],theoPp, c='b', marker="o", label='Pp(k)')
+axis.plot(success[1:N],theoPe[1:N], c='orange', marker="o", label='Pe(k)')
+
+
+font = {'fontname' : 'Times New Roman' , 'size' : 20}
+axis.legend(loc='upper right',fontsize = 20)
+#axis.set_title('avg = %1.3f' %avg + ' , var = %1.3f' %stdN ,**font)
+
+#axis.set_title('Solar flares over 1000 days',**font)
+axis.set_xlabel('successes',**font)
+axis.set_ylabel('P',**font)
+plt.xticks(fontsize = 25)
+plt.yticks(fontsize = 25)
+
+plt.show()
+
+
+
+
+'''
 
 y = totalflarecount/(sum(timediffhours))
 
@@ -45,8 +104,6 @@ secondinday = 3600*24
 #Pp = [(((y*secondinday)**k)*(exp(-y*secondinday))/factorial(k)) for k in successes[0:100] ]
 
 PB = []
-
-
 
 
 
@@ -69,6 +126,6 @@ axis.set_ylabel('P',**font)
 
 plt.show()
 
-
+'''
 
 
