bbee31668dd136c5b8dd39904431deba445cbcba
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-04-03 19:35:57 -0400

    3 regimes added and starting mean=var test

--- project draft1.py
+++ project draft1.py
@@ -25,15 +25,76 @@ minbetween = np.diff(minfrom1stflare)
 minbhist = np.histogram(minbetween,bins=100)
 
 '''
-photonrate = (1/3e8)
-timeinterval = 1e8
-lam = photonrate*timeinterval  #photons/nanosecond
-pdata = np.random.poisson(lam,1000)
-print(np.mean(pdata)**0.5 , np.std(pdata))
+'''
+emitters = 10
+detect = np.array([0,1])
+
+photondist = np.random.choice(detect , size = emitters, replace = True , p=[0.5,0.5])
+
+'''
+measurements = 10000
+
+#poisson sim
+
+photonrate = 3e8 #photon/second
+timeinterval = 1/(3e8) #seconds
+lam = photonrate*timeinterval  
+pdata = np.random.poisson(lam,measurements)
+
+pd = np.reshape(pdata,(100,100))
+
+pdbootmean = np.average(np.random.choice(pdata,[10000,20]),axis=1)
+
+pdmeanhist = np.histogram(pdbootmean, bins = 100)
+
+pstd = np.std(pdata)
+
+pdatahist = np.histogram(pdata,bins=np.max(pdata)+1)
+
+#sub poisson sim
+
+photonrate = 3e8 #photon/second
+timeinterval = 1/(1e6) #seconds
+lam = photonrate*timeinterval  
+subpdata = np.random.poisson(lam,measurements)
+
+subpd = np.reshape(subpdata,(100,100))
+
+subpdrowmean = np.average(subpd,axis=1)
+
+substd = np.std(subpdata)
+
+subpdatahist = np.histogram(subpdata,bins=np.max(subpdata)+1)
+
+
+#super poisson sim
+
+photonrate = 3e8 #photon/second
+timeinterval = 1/(1e6) #seconds
+lam = photonrate*timeinterval  
+superpdata = np.random.poisson(lam,measurements)
+
+superpd = np.reshape(superpdata,(100,100))
+
+superpdrowmean = np.average(superpd,axis=1)
+
+superstd = np.std(superpdata)
+
+superpdatahist = np.histogram(superpdata,bins=np.max(superpdata)+1)
+
+
+
 
 
 figure1, axis = plt.subplots(1, 1,constrained_layout=True)
-axis.hist(pdata,bins=100)
+#hdata, bins, bars = axis.hist(pdata,bins=np.max(pdata), align = 'mid')
+
+#x = 0.5*(pdatahist[1][1:] + pdatahist[1][:-1])
+x = np.arange(0,np.max(pdata)+1)
+#axis.plot(x , pdatahist[0] , color="red")
+axis.scatter(x,pdatahist[0], s=100, c='r', marker="o", label='poisson light')
+axis.bar(x,pdatahist[0])
+
 #axis.hist(minbetween,bins=100)
 #axis.plot(minbhist[0] , color="red")
 
@@ -57,4 +118,57 @@ plt.yticks(fontsize = 25)
 
 #axis.set_xlabel('(sample intercept)-(population intercept)',**font)
 #axis.set_ylabel('(sample slope)-(population slope)',**font)
+
+
+
+
+
+figure2, axis = plt.subplots(1, 1,constrained_layout=True)
+
+#x = np.arange(0,np.max(pdata)+1)
+#axis.plot(pdrowmean , color="red")
+axis.hist(pdmean,bins=100)
+#axis.scatter(x,pdrowmean, s=100, c='r', marker="o", label='poisson light')
+
+
+#axis.hist(minbetween,bins=100)
+#axis.plot(minbhist[0] , color="red")
+
+#plt.hist(aevalues,bins=50, weights = w)
+#axis.scatter(eb,ea, s=20, c='r', marker="o", label='exp(k)')
+#axis.scatter(successes[0:100],Pp, s=20, c='g', marker="o", label='Pp(k)')
+
+
+#axis.legend(loc='upper right')
+
+font = {'fontname' : 'Times New Roman' , 'size' : 25}
+plt.xticks(fontsize = 25)
+plt.yticks(fontsize = 25)
+
+#axis.axvline( x = upbound, color = 'b', label = '2.5% ->')
+#axis.text(upbound, np.max(aehist[0]), '2.5% ->',**font)
+#axis.axvline( x = lowbound, color = 'b', label = '<- 2.5%')
+#axis.text(lowbound, np.max(aehist[0]), '<- 2.5%', horizontalalignment='right',**font)
+#axis.set_title('avg y-axis = %1.3f' %avgeb + ' , std y-axis = %1.3f' %stdeb + ',  avg y-axis = %1.3f' %avgeb + ' , std y-axis = %1.3f' %stdeb ,**font)
+
+
+#axis.set_xlabel('(sample intercept)-(population intercept)',**font)
+#axis.set_ylabel('(sample slope)-(population slope)',**font)
+
+
+
+
+
+
+
+
+
+plt.grid()
+plt.show()
+
+
+
+
+
+
 
