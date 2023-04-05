155aa81635f33c44a6a3f0edf5cdab05fa3b7aa3
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-04-05 17:05:41 -0400

    post meeting edits

--- HW8_2_1.py
+++ HW8_2_1.py
@@ -8,39 +8,41 @@ N = 1000
 a1 = 0.7
 a2 = -0.2
 
-z = np.zeros([N,5])
+z = np.zeros([N])
+z[0] = np.random.normal(0,1,1)
+z[1] = a1*z[0] + np.random.normal(0,1,1)
 
-for i in range(0,N):
-    z[i][0] = np.random.normal(0,1,1)
-    z[i][1] = a1*z[i][0] + np.random.normal(0,1,1)
-    z[i][2] = a1*z[i][1] + a2*z[i][0] + np.random.normal(0,1,1)
-    z[i][3] = a1*z[i][2] + a2*z[i][1] + np.random.normal(0,1,1)
-    z[i][4] = a2*z[i][2] + np.random.normal(0,1,1)
+for i in range(2,N):
+    z[i] = a1*z[i-1] + a2*z[i-2] + np.random.normal(0,1,1) 
 
-c = np.zeros([N,len(z[0])])
-for x in range(0,N):
-    for k in range(0,len(z[0])):
-        #print(k,'--')
-        i = 0
-        s = 0
-        while i+k < len(z[0]):
-            s = s + (z[x][i] - np.mean(z))*(z[x][i+k] - np.mean(z))
-            #print(i, i+k)
-            i = i + 1
-        c[x][k] = s/len(z[0])
+
+    
+c = np.zeros(N)
+for k in range(0,N):
+    s = 0
+    t = 0
+    while t+k < N:
+        s = s + (z[t] - np.mean(z))*(z[t+k] - np.mean(z))
+        t = t + 1
+        if k == 1:
+            r1[t] = 
+    c[k] = s/(N-1)
         
 r = c/c[0]
 
 
-p = np.zeros(5)
+
+p = np.zeros(N)
+
+
 p[0] = 1
 p[1] = a1/(1-a2)
 p[2] = a2 + a1*p[1]
-p[3] = a1*p[2] + a2*p[1]
-p[4] = a1*p[2]
 
-r1 = r[:,0]
-rboot = np.random.choice(r1, size = N, replace = True)
+for m in range(3,N):
+    p[m] = a1*p[m-1] + a2*p[m-2]
+        
+
 
 # 99% CI -> z_0.005 -> z = +/- 2.33
 
@@ -71,13 +73,17 @@ for i in range(0,N):
 # 99% CI -> z_0.005 -> z = +/- 2.33
 
 
+#8_2_3
+
+
+
 
 
 
 figure1, axis = plt.subplots(1, 1,constrained_layout=True)
-axis.hist(wa[:,0])
-#axis.plot(r, color = "red", label='k')
-#axis.plot(p, color = "blue", label='p')
+#axis.hist()
+axis.plot(r, color = "red", label='k')
+axis.plot(p, color = "blue", label='p')
 
 
 axis.legend(loc='upper right')
