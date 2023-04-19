806a8e19bfbc96372dae94ea690a05c181871cf1
hummel2829 <124398559+hummel2829@users.noreply.github.com>
2023-04-19 12:44:41 -0400

    slope confusion

--- project_III.py
+++ project_III.py
@@ -25,8 +25,12 @@ logch1TrueTimehist = np.log(ch1TrueTimehist[0])
 
 loglinefitindex = np.nonzero(logch1TrueTimehist > -1e9) #used to skip -inf from log(0)
 
-#m,b = np.polyfit(ch1TrueTimehist[1][0:barindex[0][-1]] , logch1TrueTimehist[0:barindex[0][-1]] ,1)
+photonrate = np.sum(ch1TrueTimehist[0])/NVch1TrueTime[0][-1]
+# photonrate = 1.1335066003515445e-08 photon/picosecond
+
 m,b = np.polyfit(ch1TrueTimehist[1][loglinefitindex[0]] , logch1TrueTimehist[loglinefitindex[0]] ,1)
+#m = -1.0894839325117183e-08 , b = 11.189833490594655
+
 
 '''
 
@@ -48,7 +52,7 @@ axis.set_title('Time intervals recorded between photon counts of NV samples' ,**
 axis.set_ylabel('time between photon count (x10^9 picoseconds)',**font)
 axis.set_xlabel('interval number',**font)
 
-'''
+
 
 
 figure2, axis = plt.subplots(1, 1,constrained_layout=True)
@@ -57,8 +61,8 @@ figure2, axis = plt.subplots(1, 1,constrained_layout=True)
 #axis.scatter(xx, ch1hist[0], s=50, c='r', marker="o", label='interval count')
 barindex = np.nonzero(ch1TrueTimehist[1] < 1e9)
 
-axis.semilogy(ch1TrueTimehist[1][0:barindex[0][-1]],ch1TrueTimehist[0][0:barindex[0][-1]], color = 'blue')
-
+f = axis.semilogy(ch1TrueTimehist[1][0:barindex[0][-1]],ch1TrueTimehist[0][0:barindex[0][-1]], color = 'blue')
+ydata = f.get_ydata(orig = True)
 #axis.legend(loc='upper right',fontsize = 25)
 
 font = {'fontname' : 'Times New Roman' , 'size' : 25}
@@ -70,6 +74,8 @@ plt.ticklabel_format(axis='both', style='plain')
 
 axis.set_ylabel('counts',**font)
 axis.set_xlabel('time between photon counts (picoseconds',**font)
+
+'''
 
 
 figure3, axis = plt.subplots(1, 1,constrained_layout=True)
@@ -80,18 +86,18 @@ barindex = np.nonzero(ch1TrueTimehist[1] < 1e9)
 
 
 axis.scatter(ch1TrueTimehist[1][0:barindex[0][-1]] , logch1TrueTimehist[0:barindex[0][-1]] , s=50, c='r', marker="o", label='interval count')
-axis.plot(ch1TrueTimehist[1][loglinefitindex[0]], b + m * ch1TrueTimehist[1][loglinefitindex[0]], color="k", lw=2.5);
-#axis.legend(loc='upper right',fontsize = 25)
+axis.plot(ch1TrueTimehist[1][loglinefitindex[0]], b + m * ch1TrueTimehist[1][loglinefitindex[0]], color="k", lw=2.5, label = 'linear fit of ylog');
+axis.legend(loc='upper right',fontsize = 25)
 
 font = {'fontname' : 'Times New Roman' , 'size' : 25}
 plt.xticks(fontsize = 25)
 plt.yticks(fontsize = 25)
 plt.ticklabel_format(axis='both', style='plain')
 
-#axis.set_title('mean of counts = %1.3f' %intervalcountmean + ' , std interval counts = %1.3f' %intervalcountstd + ' ,**font)
+axis.set_title('logy = %1.3e x' %m + ' %1.3f' %b + '  ,  avg photonrate =  %1.3e' %photonrate ,**font)
 
 axis.set_ylabel('counts',**font)
-axis.set_xlabel('time between photon counts (picoseconds',**font)
+axis.set_xlabel('time between photon counts (picoseconds)',**font)
 
 
 
